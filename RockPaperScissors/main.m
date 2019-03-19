034c9e6ef1efbc400f9520578bb00bdacab7605f
Polina <polinmailbox@gmail.com>
2019-03-19 17:53:12 +0300

    Initial Commit

---
+++ RockPaperScissors/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RockPaperScissors
+//
+//  Created by Polina Dulko on 3/19/19.
+//  Copyright © 2019 Polina Dulko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
